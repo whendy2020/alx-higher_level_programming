fd4a6e57fbebf7edf2506718a1ede3b460bfc86f
Yewande Adeyeye <whendy.2020@gmail.com>
2023-03-08 03:41:39 +0100

    find last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 
 absoluteN = abs(number)
 y = absoluteN % 10
 
+if (number < 0):
+  y = -y  
 if (y > 5):
-    print("Last digit of",number ,"is",y ,"and is greater than 5")
+    print("Last digit of" ,number,"is", y, end=" and is greater than 5")
 elif (y < 6) and (y != 0):
-    print("Last digit of",number ,"is",y ,"and is less than 6 and not 0")
+    print("Last digit of" ,number,"is", y, end=" and is less than 6 and not 0")
 elif (y == 0):
-    print("Last digit is of",number ,"is",y ,"and is 0") 
-
+    print("Last digit of" ,number,"is", y, end=" and is 0")
