269aa516dd3399382117efc8bd3be51d064e8436
Yewande Adeyeye <whendy.2020@gmail.com>
2023-03-08 04:57:52 +0100

    vry commited

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ if (y > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, y))
 elif (y < 6) and (y != 0):
     print("Last digit of {} is {} and is less than 6 and not 0"
-         .format(number, y))
+          .format(number, y))
 elif (y == 0):
     print("Last digit of {} is {} and is 0".format(number, y))
