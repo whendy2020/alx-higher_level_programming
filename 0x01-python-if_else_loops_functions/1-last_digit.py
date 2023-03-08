30679d6d04a73b827b75dd8bbfbac4c8e7c92271
Yewande Adeyeye <whendy.2020@gmail.com>
2023-03-08 02:19:31 +0100

    find the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,9 @@ absoluteN = abs(number)
 y = absoluteN % 10
 
 if (y > 5):
-    print("last digit of" ,number, "is" ,y, "and is greater than 5")
+    print("Last digit of",number ,"is",y ,"and is greater than 5")
 elif (y < 6) and (y != 0):
-  print("last digit of" ,number, "is" ,y,  "and is less than 6 and not 0")
+    print("Last digit of",number ,"is",y ,"and is less than 6 and not 0")
+elif (y == 0):
+    print("Last digit is of",number ,"is",y ,"and is 0") 
+
