5332a6875d335041fdf76461fa32970f6a6a166f
Yewande Adeyeye <whendy.2020@gmail.com>
2023-03-08 01:46:31 +0100

    is a random number positive, negative or zero

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+if (number < 0):
+  print(number, end = " is negative")
+
+elif (number == 0):
+  print(number, end = " is zero")
+
+else: 
+  print(number, end = " is positive")
+print("")
+
