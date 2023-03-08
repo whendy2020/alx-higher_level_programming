9c3a679d4de2d345bd4ea47c4e7919dd0c24bde2
Yewande Adeyeye <whendy.2020@gmail.com>
2023-03-08 04:00:52 +0100

    another commit last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ y = absoluteN % 10
 if (number < 0):
     y = -y
 if (y > 5):
-    print("Last digit of", number , "is", y, end=" and is greater than 5")
+    print("Last digit of ", number, "is", y, end=" and is greater than 5")
 elif (y < 6) and (y != 0):
-    print("Last digit of", number , "is", y, end=" and is less than 6 and not 0")
+    print("Last digit of ", number, "is", y, end=" and is less than 6 and not 0")
 elif (y == 0):
-    print("Last digit of", number , "is", y, end=" and is 0")
+    print("Last digit of ", number, "is", y, end=" and is 0")
