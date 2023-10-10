46bf15785c3c675a65596f2db303e2435323144b
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-10 15:28:21 -0700

    edited max value in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     max_value = my_list[0]
     leng = len(my_list) - 1
-    if len(my_list) == 0:
+    if not my_list:
         return None
     else:
         for i in range(0, leng):
