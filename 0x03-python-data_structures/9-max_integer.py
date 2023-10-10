15b79add22fd77aa01fc9c11d78839731c21627d
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-10 15:39:06 -0700

    edited max int in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+
     leng = len(my_list) - 1
     if not my_list:
         return None
@@ -9,4 +10,4 @@ def max_integer(my_list=[]):
         for i in range(0, leng):
             if my_list[i] > max_value:
                 max_value = my_list[i]
-                return max_value
+        return max_value
