67f6ec58bb03d8c8a4b3b0ba6f954793095f4513
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-09 18:39:26 -0700

    matrix format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    space_char = ' '
     for i in matrix:
         for j in i:
-            if j == i[-1]:
-                print("{:d}".format(j), end=space_char)
-            else:
-                print("{:d}".format(j), end=space_char)
+            print("{:d}".format(j), end=' ')
         print()
