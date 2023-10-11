e2dd1d99c33df1cd78a84ad716ff7ba9dd9bce4b
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-10 18:12:48 -0700

    divisible by 2 true or false

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,8 @@
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
 
-    
     for i in range(0, len(my_list) - 1):
-        if i % 2 == 0 :
+        if i % 2 == 0:
             new_list[i] = True
         else:
             new_list[i] = False
