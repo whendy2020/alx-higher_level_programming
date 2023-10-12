68280616c89d4103ed8c60f4ae1669fb53fffbae
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-11 17:02:11 -0700

    square matrix and search and replace

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    leng = len(matrix)
+    new_matrix = matrix.copy()
+    for i in range(leng):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    return new_matrix
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    new_list = list(map(lambda x: replace if x in new_list and search == x  else x, new_list))
+    return new_list
