bf18ca39bea126ad4cdadc6a04bdc25e2f901d31
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-25 18:29:20 -0700

    getter n setter to acces =s private ins var and find area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,6 +2,7 @@
 class Square:
     """ private instance attribute size must be int"""
     def __init__(self, size=0):
+        """private instance attribute size"""
         self.__size = size
         size: int
 
@@ -9,10 +10,13 @@ class Square:
             raise TypeError("size must be an integer")
         if int(size) < 0:
             raise ValueError("size must be >= 0")
+
     def get_size(self):
+        """access the private instance variable to use outside"""
         return self.__size
-    
+
     def set_size(self, new_size):
+        """make size to be publicly accessible"""
         self.__size = new_size
 
     def area(self):
