d7353dee990f0480c462137f58729d0448f19a30
Yewande Adeyeye <whendy.2020@gmail.com>
2023-11-01 18:57:05 -0700

    print width and height using _dict_

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,12 +7,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        
-            
+
         @property
         def height(self):
             return self.__height
-        
+
         @height.setter
         def height(self, value):
             if type(value) is not int:
@@ -21,11 +20,11 @@ class Rectangle:
                 raise ValueError("height must be >= 0")
             else:
                 self.__height = value
-        
+
         @property
         def width(self):
             return self.__width
-        
+
         @width.setter
         def width(self, newvalue):
             if type(newvalue) is not int:
