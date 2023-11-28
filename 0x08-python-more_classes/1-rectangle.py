97ae5966dd8e54b8fbea27523ca15b5959c34794
Yewande Adeyeye <whendy.2020@gmail.com>
2023-11-27 18:35:58 -0800

    height and width using properties, getters and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,12 +7,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        
-            
+
         @property
         def height(self):
             return self.__height
-        
+
         @height.setter
         def height(self, value):
             if type(value) is not int:
@@ -21,16 +20,16 @@ class Rectangle:
                 raise ValueError("height must be >= 0")
             else:
                 self.__height = value
-        
+
         @property
         def width(self):
             return self.__width
-        
+
         @width.setter
         def width(self, value):
             if type(value) is not int:
                 raise TypeError("width must be an integer")
-            if newvalue < 0:
+            if value < 0:
                 raise ValueError("width must be >= 0")
             else:
                 self.__width = value
